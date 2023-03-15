bbf580e254b09b86e76f179a9de18607161937b9
chihoa-nguyen <nguyenchihoa.ptithcm.d19@gmail.com>
2023-03-15 20:29:03 +0700

    uploaded source

---
+++ QLNV/src/Controller/MyConnection.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    private static MyConnection instance;
+    private Connection connection;
+
+    private MyConnection() {
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=QUANLYNHANVIEN";
+        try {
+            connection = DriverManager.getConnection(url , "sa", "519216");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized static MyConnection getInstance() {
+        if(instance == null)
+            instance = new MyConnection();
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
